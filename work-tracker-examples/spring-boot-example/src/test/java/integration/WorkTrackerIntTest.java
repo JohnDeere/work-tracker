abe0dee244dc7356768dca6589f15eb297b2ea38
Mahesh Gaya <mmaheshgaya@gmail.com>
2019-01-23 13:24:32 -0600

    update copyright to 2019 (#18)

--- work-tracker-examples/spring-boot-example/src/test/java/integration/WorkTrackerIntTest.java
+++ work-tracker-examples/spring-boot-example/src/test/java/integration/WorkTrackerIntTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018 Deere & Company
+ * Copyright 2019 Deere & Company
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
