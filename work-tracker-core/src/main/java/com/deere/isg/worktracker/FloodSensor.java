a38a61453457f4bd377c0f8609daa83738c46e87
Avery Regier <AveryRegier@users.noreply.github.com>
2019-01-28 07:43:19 -0600

    add type of 429 restriction and retry after as metadata. (#21)
    * add type of 429 restriction and retry after as metadata.
    
    * add documentation for connection limit log metadata

--- work-tracker-core/src/main/java/com/deere/isg/worktracker/FloodSensor.java
+++ work-tracker-core/src/main/java/com/deere/isg/worktracker/FloodSensor.java
@@ -18,6 +18,7 @@
 package com.deere.isg.worktracker;
 
 import net.logstash.logback.argument.StructuredArgument;
+import net.logstash.logback.argument.StructuredArguments;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -57,7 +58,7 @@ public abstract class FloodSensor<W extends Work> {
         if (exceeds(limit, predicate) && notCheckedYet) {
             return likeThingsStream(predicate)
                     .findFirst()
-                    .map(oldestSimilar -> getRetryAfter(oldestSimilar, message));
+                    .map(oldestSimilar -> getRetryAfter(oldestSimilar, typeName, message));
         }
         return Optional.empty();
     }
@@ -86,10 +87,13 @@ public abstract class FloodSensor<W extends Work> {
         return likeThingsStream(predicate).limit(limit + 1).count() > limit;
     }
 
-    private int getRetryAfter(W oldestSimilar, String message) {
+    private int getRetryAfter(W oldestSimilar, String typeName, String message) {
+        outstanding.putInContext("limit_type", typeName);
         List<StructuredArgument> metadata = outstanding.getCurrentMetadata();
+        int retryAfterSeconds = (int) Math.ceil(oldestSimilar.getElapsedMillis() / 1000.0);
+        metadata.add(StructuredArguments.keyValue("retry_after_seconds", retryAfterSeconds));
         logger.warn(message, metadata.toArray());
-        return (int) Math.ceil(oldestSimilar.getElapsedMillis() / 1000.0);
+        return retryAfterSeconds;
     }
 
     protected void setLogger(Logger logger) {
