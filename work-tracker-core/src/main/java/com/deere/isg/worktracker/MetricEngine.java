3c2b6fa290d90483ae8be8b8c15b47c911e697a3
Avery Regier <regieraveryj@johndeere.com>
2019-02-25 15:41:11 -0600

    std dev changes

--- work-tracker-core/src/main/java/com/deere/isg/worktracker/MetricEngine.java
+++ work-tracker-core/src/main/java/com/deere/isg/worktracker/MetricEngine.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2019 Deere & Company
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.deere.isg.worktracker;
 
 import org.joda.time.Instant;
@@ -137,7 +153,9 @@ public interface MetricEngine<W extends Work> extends PostProcessor<W> {
                     new NumberMetricReport("average", longSummaryStatistics.getAverage()),
                     new NumberMetricReport("max", longSummaryStatistics.getMax()),
                     new NumberMetricReport("min", longSummaryStatistics.getMin()),
-                    new NumberMetricReport("std_dev", longSummaryStatistics.getStandardDeviation())
+                    new NumberMetricReport("std_dev_pop", longSummaryStatistics.getStandardDeviation()),
+                    new NumberMetricReport("var_pop", longSummaryStatistics.getVariance()),
+                    new NumberMetricReport("count", longSummaryStatistics.getCount())
             ).collect(Collectors.toList());
         }
 
--- work-tracker-spring/src/main/java/com/deere/isg/worktracker/spring/DefaultSpringMetrics.java
+++ work-tracker-spring/src/main/java/com/deere/isg/worktracker/spring/DefaultSpringMetrics.java
@@ -1,17 +1,30 @@
+/**
+ * Copyright 2019 Deere & Company
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.deere.isg.worktracker.spring;
 
 import com.deere.isg.worktracker.DefaultMetricEngine;
 import com.deere.isg.worktracker.MetricEngine;
 import com.deere.isg.worktracker.RootCauseTurboFilter;
-import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.OptionalPropertyMode;
 import org.joda.time.Duration;
 import org.slf4j.MDC;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
